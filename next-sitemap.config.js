59322cc8b3b69ce2210f853f0a0416f6330fab50
JeanR1802 <rs.cedora@gmail.com>
2025-08-27 18:40:17 -0700

    Fix: Nueva Version6.5

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://gestularia.com', // tu dominio principal
+  generateRobotsTxt: true,           // genera robots.txt automáticamente
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
